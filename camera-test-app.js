cd491a8a0c3ee3c7b1575b23631a4a37ea454ca2
Andrew <andrewfribush@gmail.com>
2015-05-23 22:07:04 -0400

    Beautified

--- camera-test-app.js
+++ camera-test-app.js
@@ -2,44 +2,54 @@ Pictures = new Mongo.Collection("pictures");
 
 if (Meteor.isClient) {
 
- Template.pictures.helpers({
- pictures: function () {
- return Pictures.find({}, {sort: {'date_added': -1}, limit: 50});
- }
- });
+    Template.pictures.helpers({
+        pictures: function() {
+            return Pictures.find({}, {
+                sort: {
+                    'date_added': -1
+                },
+                limit: 50
+            });
+        }
+    });
 
- Template.pictures.rendered = function() {
- var video = document.querySelector('video');
- var canvas = document.querySelector('canvas');
- var ctx = canvas.getContext('2d');
- var localMediaStream = null;
+    Template.pictures.rendered = function() {
+        var video = document.querySelector('video');
+        var canvas = document.querySelector('canvas');
+        var ctx = canvas.getContext('2d');
+        var localMediaStream = null;
 
- function snapshot() {
- if (localMediaStream) {
- ctx.drawImage(video, 0, 0, 400, 150);
- Pictures.insert({url: canvas.toDataURL('image/png'), date_added: new Date});
- video.src = window.URL.createObjectURL(localMediaStream);
- }
- }
+        function snapshot() {
+            if (localMediaStream) {
+                ctx.drawImage(video, 0, 0, 400, 150);
+                Pictures.insert({
+                    url: canvas.toDataURL('image/png'),
+                    date_added: new Date
+                });
+                video.src = window.URL.createObjectURL(localMediaStream);
+            }
+        }
 
- video.addEventListener('click', snapshot, false);
+        video.addEventListener('click', snapshot, false);
 
- navigator.getUserMedia = navigator.getUserMedia ||
- navigator.webkitGetUserMedia ||
- navigator.mozGetUserMedia ||
- navigator.msGetUserMedia;
+        navigator.getUserMedia = navigator.getUserMedia ||
+            navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia ||
+            navigator.msGetUserMedia;
 
- navigator.getUserMedia({video: true}, function(stream) {
- video.src = window.URL.createObjectURL(stream);
- localMediaStream = stream;
- }, function(){
- console.log("error getting image");
- });
- }
+        navigator.getUserMedia({
+            video: true
+        }, function(stream) {
+            video.src = window.URL.createObjectURL(stream);
+            localMediaStream = stream;
+        }, function() {
+            console.log("error getting image");
+        });
+    }
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function() {
+        // code to run on server at startup
+    });
 }

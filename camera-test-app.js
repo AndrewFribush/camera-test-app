22e0264b2b16c6410ad8e017254f3589a9a3c802
Andrew <andrewfribush@gmail.com>
2015-05-23 21:15:23 -0400

    Working, no css though

--- camera-test-app.js
+++ camera-test-app.js
@@ -1,19 +1,41 @@
+Pictures = new Mongo.Collection("pictures");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+ Template.pictures.helpers({
+ pictures: function () {
+ return Pictures.find({}, {sort: {'date_added': -1}, limit: 50});
+ }
+ });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+ Template.pictures.rendered = function() {
+ var video = document.querySelector('video');
+ var canvas = document.querySelector('canvas');
+ var ctx = canvas.getContext('2d');
+ var localMediaStream = null;
+
+ function snapshot() {
+ if (localMediaStream) {
+ ctx.drawImage(video, 0, 0, 400, 150);
+ Pictures.insert({url: canvas.toDataURL('image/png'), date_added: new Date});
+ video.src = window.URL.createObjectURL(localMediaStream);
+ }
+ }
+
+ video.addEventListener('click', snapshot, false);
+
+ navigator.getUserMedia = navigator.getUserMedia ||
+ navigator.webkitGetUserMedia ||
+ navigator.mozGetUserMedia ||
+ navigator.msGetUserMedia;
+
+ navigator.getUserMedia({video: true}, function(stream) {
+ video.src = window.URL.createObjectURL(stream);
+ localMediaStream = stream;
+ }, function(){
+ console.log("error getting image");
+ });
+ }
 }
 
 if (Meteor.isServer) {
